7ef9f447facafd1aa939668e01418e64780886b9
Dhine Franklin Arjona <76668292+Dhinearjona@users.noreply.github.com>
2022-11-25 08:11:29 +0800

    a

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+    var swiper2 = new Swiper(".mySwiper2", {
+        direction: "vertical",
+        spaceBetween: 50,
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
